109bfac141e75713e207653b6ccb1649fa964282
Colin Mackay <hello@xander-framework.org>
2013-02-13 21:47:38 +0000

    Added main project and test suite project
    Added the main Xander.PasswordValidator project and its unit test suite
    project (using NUnit)

---
+++ src/Xander.PasswordValidator.TestSuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xander.PasswordValidator.TestSuite")]
+[assembly: AssemblyDescription("The test suite for the Xander.PasswordValidator assembly")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xander.PasswordValidator")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Colin Angus Mackay")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6816c334-dd31-4d7e-8064-72d83af8ee22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
