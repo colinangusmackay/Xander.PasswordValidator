0359fd964b243e76b670a7082dc0e0e7c357c81d
Colin Mackay <hello@xander-framework.org>
2013-02-13 23:58:49 +0000

    closes #1 : Validator checks password length
    The validator checks the password length and returns false if the length
    is too short.

---
+++ src/Xander.PasswordValidator.TestSuite/ValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+namespace Xander.PasswordValidator.TestSuite
+{
+    [TestFixture]
+    public class ValidatorTests
+    {
+        private Validator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new Validator(8);
+        }
+        [Test]
+        public void Validate_1CharacterPassword_FailValidation()
+        {
+            bool actualResult = validator.Validate("1");
+
+            Assert.AreEqual(false, actualResult);
+        }
+
+        [Test]
+        public void Validate_10CharacterPassword_PassValidation()
+        {
+            bool actualResult = validator.Validate("1234567890");
+
+            Assert.AreEqual(true, actualResult);
+        }
+
+        [Test]
+        public void Validate_8CharacterPassword_PassValidaton()
+        {
+            bool actualResult = validator.Validate("12345678");
+
+            Assert.AreEqual(true, actualResult);
+        }
+    }
+}
---
+++ src/Xander.PasswordValidator/Validator.cs
@@ -0,0 +1,18 @@
+﻿namespace Xander.PasswordValidator
+{
+    public class Validator
+    {
+        private readonly int _minPasswordLength;
+        public Validator(int minPasswordLength)
+        {
+            _minPasswordLength = minPasswordLength;
+        }
+
+        public bool Validate(string password)
+        {
+            if (password.Length >= _minPasswordLength)
+                return true;
+            return false;
+        }
+    }
+}
