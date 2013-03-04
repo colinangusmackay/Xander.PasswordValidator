86f620b79fa3e681a0478c3c3c0a7c6b9456f970
Colin Mackay <hello@xander-framework.org>
2013-03-04 21:48:44 +0000

    Added a demo of the basic validator
    Added a demo of the basic validator

---
+++ src/Xander.Demo.PasswordValidator.Web.Mvc3/Models/ManuallyRunningTheValidatorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Xander.Demo.PasswordValidator.Web.Mvc3.Models
+{
+  public class ManuallyRunningTheValidatorModel 
+  {
+    public string Password { get; set; }
+
+    public bool IsPasswordAcceptable { get; set; }
+  }
+}
