77cb11705f936f28c65b027f6a9eef250e3adc64
Colin Mackay <hello@xander-framework.org>
2013-08-25 01:31:59 +0100

    Added validator for letters - NOTE: At this point it only checks for Latin1 letters (which includes ASCII), unlike the server side implementation.

--- src/Xander.Demo.PasswordValidator.Web.Mvc3/Scripts/jquery.validate.xanderpassword.1.1.0.js
+++ src/Xander.Demo.PasswordValidator.Web.Mvc3/Scripts/jquery.validate.xanderpassword.1.1.0.js
@@ -42,8 +42,12 @@ jQuery(document).ready(function () {
   );
 
   jQuery.validator.addMethod(
-    "xander-needs-letter",
+    "xanderNeedsLetter",
     function (value, element, parameters) {
+      if (this.optional(element))
+        return true;
+      var lettersRegEx = /[a-zA-Z\u00AA\u00B5\u00BA\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF\u00D0\u00D1\u00D2\u00D3\u00D4\u00D5\u00D6\u00D8\u00D9\u00DA\u00DB\u00DC\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u00E6\u00E7\u00E8\u00E9\u00EA\u00EB\u00EC\u00ED\u00EE\u00EF\u00F0\u00F1\u00F2\u00F3\u00F4\u00F5\u00F6\u00F8\u00F9\u00FA\u00FB\u00FC\u00FD\u00FE\u00FF]/;
+      return lettersRegEx.test(value);
     },
     "The password requires a letter."
   );
---
+++ src/Xander.PasswordValidator.BuildTools.Letters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xander.PasswordValidator.BuildTools.Letters
+{
+    class Program
+    {
+        public const int startIndex = 128;
+        public const int lastIndex = 255;
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[a-zA-Z");
+            for (int i = startIndex; i <= lastIndex; i++)
+            {
+                char c = (char) i;
+                if (char.IsLetter(c))
+                {
+                    sb.Append("\\u");
+                    sb.Append(i.ToString("X4"));
+                }
+            }
+            sb.Append("]");
+            Console.WriteLine("Done!");
+            string result = sb.ToString();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
